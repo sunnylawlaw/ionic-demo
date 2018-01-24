9bf7a3ec250cd9b42844393e1aa6fba03aad4688
Sunny Law <sunny.law@gtomato.com>
2018-01-24 19:29:46 +0800

    register form submit

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -14,14 +14,18 @@ import { HomePage } from '../home/home';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  private username: string;
-  private password: string;
-  private confirmPassword: string;
-  private tel: number;
-  private email: string;
-  private address: string;
+  private _username: string;
+  private _password: string;
+  private _confirmPassword: string;
+  private _tel: number;
+  private _email: string;
+  private _address: string;
 
   private _navCtrl: NavController;
+
+  formData = <any>{};
+
+
 
   constructor(_navCtrl: NavController, public navParams: NavParams) {
     this._navCtrl = _navCtrl;
@@ -35,35 +39,40 @@ export class RegisterPage {
     this._navCtrl.pop();
   }
 
+
   check() {
-    if (this.password != this.confirmPassword) {
+    console.log(this.formData);
+
+    this._username=this.formData.username;
+
+    if (this.formData.password != this.formData.confirmPassword) {
       alert('Please input same Password');
     } else if (
-      this.username == undefined ||
-      this.password == undefined ||
-      this.confirmPassword == undefined ||
-      this.tel == undefined ||
-      this.email == undefined ||
-      this.address == undefined) {
-      alert('Please Fill All');
+      this.formData.username == undefined ||
+      this.formData.password == undefined ||
+      this.formData.confirmPassword == undefined ||
+      this.formData.tel == undefined ||
+      this.formData.email == undefined ||
+      this.formData.address == undefined) {
+      // alert('Please Fill All');
     } else {
       this.createAC();
     }
   }
   createAC() {
     console.log('ok');
-    console.log(this.username);
-    console.log(this.password);
-    console.log(this.tel);
-    console.log(this.email);
-    console.log(this.address);
-    this._navCtrl.push(HomePage, {
-      res_username: this.username,
-      res_password: this.password,
-      res_tel: this.tel,
-      res_email: this.email,
-      res_address: this.address
-    });
+    // console.log(this.username);
+    // console.log(this.password);
+    // console.log(this.tel);
+    // console.log(this.email);
+    // console.log(this.address);
+    // this._navCtrl.push(HomePage, {
+    //   res_username: this.username,
+    //   res_password: this.password,
+    //   res_tel: this.tel,
+    //   res_email: this.email,
+    //   res_address: this.address
+    // });
   }
 
 }
